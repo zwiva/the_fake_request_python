abf6b1da61663e6fd4f3c422b0108a3d4fff9f44
Andrea Castro <ap.castrocastellon@gmail.com>
2020-06-19 20:05:04 -0400

    version menu

--- fake_request.py
+++ fake_request.py
@@ -3,13 +3,16 @@
 import json
 import requests
 
-# ---------- d 1
 test_url = "https://reqres.in/api/users/"
-method = "GET"
-# method = "POST"
-# method = "PUT"
-# method = "DELETE"
-# 
+
+opcion = int(input("""Bienvenido. Escoja una opción: 
+1 para mostrar el tiṕo de respuesta por defecto del programa,
+2 para ver 3 usuarios,
+3 para crear un usuario, 
+4 para actualizar un usuario, 
+5 para eliminar un usuario,
+6 para salir \n"""))
+
 def request(method, url, payload=""):
     payload = {}
     headers= {}
@@ -17,30 +20,71 @@ def request(method, url, payload=""):
     results = response.text
     if method != "DELETE":
         results = json.loads(response.text) #si, no es gran cosa, cambia las comillas, import json decoder requiere de un valor     
-    print("codigo de respuesta:",response)
+    print(response)
 #   print(results)
-    print(type(results))
+    # print(type(results))
     return results
 
-results = request(method, test_url)
+while opcion != 6:
+    if opcion == 1: # mostrar tipo de respuesta
+        print("Por defecto el programa usa GET")
+        method = "GET"
+        results = request(method, test_url)
+        opcion = int(input("Escoge otra opcion o 6 para salir"))
+    elif opcion == 2:
+        method = "GET"
+        results = request(method, test_url)
+        def show_users(dictionary_users):
+            for i in range(0,3):
+                print(dictionary_users["data"][i]["id"], dictionary_users["data"][i]["first_name"], dictionary_users["data"][i]["last_name"])
+        show_users(results)
+        opcion = int(input("Escoge otra opcion o 6 para salir"))
+    elif opcion == 3:
+        print("Escogiste crear un usuario")
+        method = "POST"
+        results = request(method, test_url)      
+        def create_user():
+            print("jajajaja, ya quisiera yo saber hacer eso")
+        create_user()
+        
+        opcion = int(input("Escoge otra opcion o 6 para salir"))        
+    
+    elif opcion == 4:
+        print("Escogiste actualizar un usuario")
+        method = "PUT"
+        results = request(method, test_url)
 
-# request(method,url,payload)
+        def update_user():
+            print("...")
 
-# ---------- d 2
-def show_users(dictionary_users):
-    for i in range(0,3):
-        print(dictionary_users["data"][i]["id"], dictionary_users["data"][i]["first_name"], dictionary_users["data"][i]["last_name"])
+        print("")
+    
+        opcion = int(input("Escoge otra opcion o 6 para salir"))
+    elif opcion == 5:
+        print("Escogiste eliminar un usuario:")
 
-show_users(results)
+        def delete_user():
+            print("...")
 
-# ---------- d 3
+        method = "DELETE"
+        print("")
 
-def create_user():
-    print("jajajaja, ya quisiera yo saber hacer eso")
-
-create_user()
-# ---------- d 4
-# ---------- d 5
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        opcion = int(input("Escoge otra opcion o 6 para salir"))    
+    elif opcion == 6: 
+        print("Salir") 
+    else:
+        print("Opción inválida, adiós!")
+        opcion = 6
 
 
 # API algo que se consulta, es una url a la que uno le pide cosas y te retorna una respuesta, es una forma de comunicarse con otro computador 
